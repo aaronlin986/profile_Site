a94a0b1fe86cc326f638274232215a15fa3061ed
aaronlin986 <46300650+aaronlin986@users.noreply.github.com>
2020-11-16 18:00:09 -0500

    Changed background, added svg navigation menu, formatted sections, added template to about section

---
+++ script.js
@@ -0,0 +1,34 @@
+var items = ["section1", "section2", "section3", "section4"];
+
+function update(item, order) {
+    for (i = 0; i < items.length; i++) {
+        if (item != items[i]) {
+            var text = document.getElementById(items[i]);
+            text.style.fill = "#BEBEBE";
+            text.setAttribute("font-size", "1em");
+            text.setAttribute("font-weight", "normal");
+            text.setAttribute("selected", "false");
+
+            var circle = document.getElementById(i);
+            circle.style.fill = "#BEBEBE";
+            circle.setAttribute("r", "10");
+        }
+    }
+}
+
+function activate(item, order) {
+    var text = document.getElementById(item);
+    text.style.fill = "black";
+    text.setAttribute("font-size", "1.5em");
+    text.setAttribute("font-weight", "bold");
+    text.setAttribute("selected", "true");
+
+    var circle = document.getElementById(order);
+    circle.style.fill = "black";
+    circle.setAttribute("r", "5");
+
+    var circleSelect = document.getElementById("active");
+    circleSelect.style.cy = circle.getAttribute("cy");
+
+    update(item, order);
+}
